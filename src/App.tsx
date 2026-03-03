32d2c4263534fb810d7f0984163b5180c583cee1
Zer0del <65867146+Zer0del@users.noreply.github.com>
2026-03-03 23:12:35 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,31 @@ import React from 'react';
 import './App.css';
 
 import { Home } from './pages/Home';
+import { Tournaments } from './pages/Tournaments';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Header } from './components/Header';
-import { Plan } from './pages/Plan';
-import { Tournaments } from './pages/Tournaments';
+import { Footer } from './components/Footer';
 
 function App() {
-
   return (
-
     <BrowserRouter>
-
       <div className="bg-black flex justify-center">
-
         <div className="w-full bg-black text-white h-screen font-bold flex flex-col max-w-xl">
           <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/tournaments" element={<Tournaments />} />
-            <Route path="/mybets" element={<div className="p-4 text-center text-2xl text-white">Мои ставки</div>} />
-            <Route path="/wallet" element={<div className="p-4 text-center text-2xl text-white">Кошелёк (баланс TON + вывод)</div>} />
-            <Route path="/plan" element={<Plan />} />
-          </Routes>
+          
+          <main className="flex-1 overflow-y-auto pb-20">   {/* ← это важно */}
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/tournaments" element={<Tournaments />} />
+              <Route path="/mybets" element={<div className="p-4 text-center text-2xl text-white">Мои ставки (скоро)</div>} />
+              <Route path="/wallet" element={<div className="p-4 text-center text-2xl text-white">Кошелёк</div>} />
+            </Routes>
+          </main>
 
+          <Footer />
         </div>
       </div>
     </BrowserRouter>
-
   )
 }
 
