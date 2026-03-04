1f6ff436b1b112a26290ad1581fb612119e0234a
Zer0del <65867146+Zer0del@users.noreply.github.com>
2026-03-04 15:57:48 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,8 @@ import { Home } from './pages/Home';
 import { Tournaments } from './pages/Tournaments';
 import { MyBets } from './pages/MyBets';
 import { Admin } from './pages/Admin';
-import { useTelegram } from './hooks/useTelegram';
 
 function App() {
-  const { tg } = useTelegram();
-
-  // Проверка админа (твой ID)
-  const isAdmin = tg.initDataUnsafe?.user?.id?.toString() === '636499517';
-
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-[var(--tg-theme-bg-color)] text-[var(--tg-theme-text-color)]">
@@ -19,7 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/tournaments" element={<Tournaments />} />
           <Route path="/my-bets" element={<MyBets />} />
-          {isAdmin && <Route path="/admin" element={<Admin />} />}
+          <Route path="/admin" element={<Admin />} />
         </Routes>
       </div>
     </BrowserRouter>
