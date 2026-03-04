6c1e8e72df8ae878046d4c84aacc8a9a3a42ee52
Zer0del <65867146+Zer0del@users.noreply.github.com>
2026-03-04 12:39:54 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react()],
+  server: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  },
+  build: {
+    // Улучшаем отдачу статических файлов
+    assetsInlineLimit: 0,
+    chunkSizeWarningLimit: 1000,
+  },
+  // Это важно для TON Connect
   define: {
-    __APP_VERSION__: JSON.stringify('v1.0.0'),
-    // __CONTRACT_ADDRESS__: JSON.stringify('EQAaQatn2OVF_He6r-TSJX1g5Qc1vfKj6Jo9Y6NZgZqy_j43'),
-    __CONTRACT_ADDRESS__: JSON.stringify(''),
-    __ADMIN_ADDRESS__: JSON.stringify(''),
-    __HOME_URL__: JSON.stringify('https://tonpanda.com/'),
-    __HOME_TG_LINK__: JSON.stringify('t.me/tonchina'),
+    'process.env': {},
   },
 })
