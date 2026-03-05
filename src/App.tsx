6c674ca11c07790b07406a0a29a54107216c9bb2
Zer0del <65867146+Zer0del@users.noreply.github.com>
2026-03-05 13:02:40 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
 import { Tournaments } from './pages/Tournaments';
 import { MyBets } from './pages/MyBets';
+import { Wallet } from './pages/Wallet';
 import { Admin } from './pages/Admin';
-import { Footer } from './components/Footer';
+
+const ADMIN_TG_ID = '636499517'; // твой Telegram ID
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-[var(--tg-theme-bg-color)] text-[var(--tg-theme-text-color)] pb-20">
+      <div className="min-h-screen bg-black text-white pb-20">
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/tournaments" element={<Tournaments />} />
           <Route path="/my-bets" element={<MyBets />} />
+          <Route path="/wallet" element={<Wallet />} />
           <Route path="/admin" element={<Admin />} />
         </Routes>
-        
         <Footer />
       </div>
     </BrowserRouter>
